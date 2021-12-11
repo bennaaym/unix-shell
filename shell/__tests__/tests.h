9a209e566e296def9659b25333e2422cd84cad03
bennaaym <mr.aymen.bennabi@gmail.com>
2021-12-11 18:08:44 +0300

    bennaaym/shell: unittests added for parser

---
+++ shell/__tests__/tests.h
@@ -0,0 +1,19 @@
+/**
+ * @file tests.h
+ * @author Aymen Bennabi
+ * @brief  Holds the blueprints of the different unittests
+ * @version 0.1
+ * @date 2021-12-11
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef TESTS_H
+#define TESTS_H
+
+void test_parser_command_max_chars();
+void test_parser_command_max_words();
+void test_parser_parse_command_with_args();
+void test_parser_parse_command_with_multiple_arbitrary_spaces();
+
+#endif
